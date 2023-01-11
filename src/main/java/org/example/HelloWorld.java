6a3ae7517eb8e44479924aeafaae275ec2b5d042
Annalee <jungsolee97@gmail.com>
2023-01-11 22:20:56 +0900

    윤호를 제거함

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,6 +3,5 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!\n");
-        System.out.println("yunho");
     }
 }
