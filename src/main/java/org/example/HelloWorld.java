c860603eb9489a52e263556a386c13dc2a81e1e7
Annalee <jungsolee97@gmail.com>
2023-01-11 22:19:46 +0900

    initial

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n");
+        System.out.println("yunho");
+    }
+}
